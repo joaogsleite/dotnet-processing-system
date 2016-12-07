a96302b10189efea59dfa7533e628bd3ea6a53a2
joaaoleite <joaogsleite@gmail.com>
2016-12-07 02:47:53 +0000

    Tuple date, id and origin

--- LibDADStorm/Tuple.cs
+++ LibDADStorm/Tuple.cs
@@ -1,29 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace DADStorm{
 
 	[Serializable()]
 	public class Tuple : EventArgs {
 
-		List<string> items;
+		private List<string> items;
+        public DateTime date;
+        public string id;
+        public IReplica origin;
+
+        public void init() {
+            this.id = RandomString();
+            this.date = DateTime.Now;
+        }
 
 		public Tuple(params string[] args){
 			items = new List<string>(args);
+            init();
 		}
 
         public Tuple() {
             items = new List<string>();
+            init();
         }
 
         public Tuple(List<string> input) {
             this.items = input;
+            init();
         }
 
         public Tuple(IList<string> input) {
             this.items = new List<string>();
             foreach(string item in input)
                 this.items.Add(item);
+            init();
         }
 
         public List<string> toList() {
@@ -33,6 +46,7 @@ namespace DADStorm{
         public void Merge(Tuple t) {
             for (int i = 1; i <= t.Count(); i++)
                 items.Add(t.Get(i));
+            init();
         }
 
         public string Get(int index){
@@ -45,6 +59,7 @@ namespace DADStorm{
 
         public void RemoveField(int index) {
             items.RemoveAt(index - 1);
+            init();
         }
 
         public int Count() {
@@ -53,10 +68,21 @@ namespace DADStorm{
 
 		public void Add(string item){
 			items.Add(item);
+            init();
 		}
 
 		public override string ToString(){
 			return string.Join(",", items.ToArray());
 		}
-	}
+
+        private string RandomString() {
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < 20; i++) {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * new Random((int)DateTime.Now.Ticks).NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+    }
 }
